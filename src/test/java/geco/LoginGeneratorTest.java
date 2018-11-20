a00127f889b59be19ce40901ebf4658dbdafbafd
randriamalala <randriamalal@gmail.com>
2018-11-20 17:28:27 +0100

    Test unitaire de login generator comme prévu dans issue #2 et fix #3

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,48 @@
+package geco;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private static LoginService loginService;
+    private static LoginGenerator loginGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR",
+                "JDU", "JRAL", "JRAL1"});
+        loginGenerator = new LoginGenerator(loginService);
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom() {
+        Assert.assertEquals("PDUR est le login genere pour Paul Durand",
+                "PDUR", loginGenerator.generateLoginForNomAndPrenom("Durand",
+                 "Paul"       ));
+    }
+
+    @Test
+    public void generateLoginForJRAL2() {
+        Assert.assertEquals("JRAL2 devrait être le login généré",
+                "JRAL2", loginGenerator.generateLoginForNomAndPrenom("Ralling",
+                        "John"));
+    }
+
+    @Test
+    public void generateLoginJROL1() {
+        Assert.assertEquals("JROL1 est le login généré pour Jean Rolling",
+                "JROL1", loginGenerator.generateLoginForNomAndPrenom("Rolling", "Jean"));
+    }
+
+    @Test
+    public void generateLoginPDUR() {
+        Assert.assertEquals("PDUR est le login généré pour Paul Dùrand",
+                "PDUR", loginGenerator.generateLoginForNomAndPrenom("Dùrand"
+                        , "Paul"));
+    }
+}
