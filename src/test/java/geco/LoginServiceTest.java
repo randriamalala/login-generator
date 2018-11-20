8fc62da0e5cb22889ab33a09647a80c1fecf93fe
randriamalala <randriamalal@gmail.com>
2018-11-20 16:03:46 +0100

    Ajout fichier de tests pour LoginService et supprime le fichier AppTest qui ne sert plus à rien

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,34 @@
+package geco;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void loginExists() {
+    }
+
+    @Test
+    public void addLogin() {
+    }
+
+    @Test
+    public void findAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void findAllLogins() {
+    }
+}
